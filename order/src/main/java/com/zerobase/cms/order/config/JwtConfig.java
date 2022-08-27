9566a1277afc05f496e5acf96f33c3ce2b6b00d6
Shin seongsu <tlstjdtn321@naver.com>
2022-08-28 01:38:53 +0900

    feat: [008-init-order-api] order-api 초기세팅

---
+++ order/src/main/java/com/zerobase/cms/order/config/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.zerobase.cms.order.config;
+
+import com.zerobase.domain.config.JwtAuthenticationprovider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JwtAuthenticationprovider jwtAuthenticationprovider() {
+        return new JwtAuthenticationprovider();
+    }
+
+}
