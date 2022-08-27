124864d7dd9569a3e59408b3036be32cee90a669
Shin seongsu <tlstjdtn321@naver.com>
2022-08-28 04:33:37 +0900

    feat: [009-PRODUCT-ADD] Product ADD, ProductItem Add
    추가로 Exception에 대한 Response 추가

---
+++ order/src/main/java/com/zerobase/cms/order/SellerProductController.java
@@ -0,0 +1,39 @@
+package com.zerobase.cms.order;
+
+import com.zerobase.cms.order.domain.product.AddProductForm;
+import com.zerobase.cms.order.domain.product.AddProductItemForm;
+import com.zerobase.cms.order.domain.product.ProductDto;
+import com.zerobase.cms.order.service.ProductItemService;
+import com.zerobase.cms.order.service.ProductService;
+import com.zerobase.domain.config.JwtAuthenticationprovider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/seller/product")
+@RequiredArgsConstructor
+public class SellerProductController {
+    private final ProductService productService;
+    private final ProductItemService productItemService;
+    private final JwtAuthenticationprovider provider;
+
+    @PostMapping
+    public ResponseEntity<ProductDto> addProduct(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+        @RequestBody AddProductForm form) {
+
+        return ResponseEntity.ok(ProductDto.from(productService.addProduct(provider.getUserVo(token).getId(), form)));
+    }
+
+    @PostMapping("/item")
+    public ResponseEntity<ProductDto> addProductItem(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+        @RequestBody AddProductItemForm form) {
+
+        return ResponseEntity.ok(ProductDto.from(productItemService.addProductItem(provider.getUserVo(token).getId(), form)));
+    }
+
+}
--- order/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
+++ order/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -8,8 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
+    NOT_FOUND_PRODUCT(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다."),
+    SAME_ITEM_NAME(HttpStatus.BAD_REQUEST, "아이템명 중복입니다."),
+    
     ALREADY_REGISTER_USER(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다.");
-
+    
+    
     private final HttpStatus httpStatus;
     private final String detail;
 }
